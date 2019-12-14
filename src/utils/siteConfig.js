c9853de8733dda02ef2cb4a8ca1d21a94d32fcc6
Panos Tsilopoulos <tsilopoulos>
2019-12-13 23:42:46 -0500

    Add favicon

--- src/utils/siteConfig.js
+++ src/utils/siteConfig.js
@@ -1,7 +1,7 @@
 module.exports = {
     siteUrl: `https://panos.tech`, // Site domain. Do not include a trailing slash!
 
-    postsPerPage: 1, // Number of posts shown on paginated pages (changes this requires sometimes to delete the cache)
+    postsPerPage: 3, // Number of posts shown on paginated pages (changes this requires sometimes to delete the cache)
 
     siteTitleMeta: `Panos Tech`, // This allows an alternative site title for meta data for pages.
     siteDescriptionMeta: `Stories, thoughts and ideas from the technology trenches`, // This allows an alternative site description for meta data for pages.
